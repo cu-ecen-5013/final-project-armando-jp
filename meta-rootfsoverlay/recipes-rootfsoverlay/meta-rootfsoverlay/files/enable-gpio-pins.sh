c412973ad19c3840afcd1bfeb5bef6a79bb2ec6b
Armando Pinales <armandop4597@gmail.com>
2021-04-06 23:07:31 -0500

    basic shell script to enable GPIO on BBB

---
+++ meta-rootfsoverlay/recipes-rootfsoverlay/meta-rootfsoverlay/files/enable-gpio-pins.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+#enable pin 27 on P9 as an output and set to 1 (HIGH)
+
+echo 115 > /sys/class/gpio/export
+echo out > /sys/class/gpio/gpio115/direction
+echo 1 > /sys/class/gpio/gpio115/value
